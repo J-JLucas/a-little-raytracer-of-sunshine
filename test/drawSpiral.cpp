7bb73c886ded3884c9f123d7d5e3a6b7a715e241
JoSh <jlucas205@proton.me>
2024-02-09 12:50:00 -0700

    idk

--- test/drawSpiral.cpp
+++ test/drawSpiral.cpp
@@ -10,11 +10,11 @@ Tuple red = Tuple::Colour(1, 0, 0);
 
 void tick(int t, Tuple &p, Canvas &c)
 {
-  float a = 0.1f; // spacing constant
+  float a = 2; // spacing constant
 
   // update point position
-  p.x = a * t * std::cos(t) + width / 2;
-  p.y = a * t * std::sin(t) + height / 2;
+  p.x = a * (t * std::cos(t)) + width / 2;
+  p.y = a * (t * std::sin(t)) + height / 2;
 
   // draw projectile on canvas
   c.writePixel(std::round(p.x), height - std::round(p.y), red);
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
   std::cout << "Position: (" << std::round(p.x) << ", " << std::round(p.y)
             << ", " << std::round(p.z) << ")" << std::endl;
 
-  while (t++ < 1000) {
+  while (t++ < 10000) {
     tick(t, p, c);
     std::cout << "Position: (" << std::round(p.x) << ", " << std::round(p.y)
               << ", " << std::round(p.z) << ")" << std::endl;
