b966b67a73dc2048a27e6f93c64c8ce44806284b
JoSh <jlucas205@proton.me>
2024-04-22 19:10:13 -0600

    started Matrix tests

--- test/testMatrix.cpp
+++ test/testMatrix.cpp
@@ -1,3 +1,17 @@
 #include "testMatrix.h"
+#include "../src/Matrix.h"
 
-void testMatrix() { return; }
+void testMatrix()
+{
+  /* Matrix A
+i \ j
+  |  1  |  2  |  3  |  4  |
+  | 5.5 | 6.5 | 7.5 | 8.5 |
+  |  9  |  10 | 11  | 12  |
+  | 13.5| 14.5| 15.5| 16.5|
+  */
+
+  Matrix A;
+
+  return;
+}
