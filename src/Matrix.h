0d63091c04f76b84800305fd265b6eeb0efbc17f
JoSh <jlucas205@proton.me>
2024-06-29 16:24:19 -0600

    matrix constructor complete, can initialize arbitrary number of elements

--- src/Matrix.h
+++ src/Matrix.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <cassert>
+#include <initializer_list>
 
 class Matrix {
 private:
@@ -12,11 +13,17 @@ private:
   float *data;
 
 public:
-  Matrix(int dimension) : n(dimension)
+  Matrix(int dimension, std::initializer_list<float> initializer_list = {})
+      : n(dimension)
   {
     assert(n >= 2 && n <= 4 && "Matrix dimensions must be between 2 and 4");
-    data = new float[n * n];
+    data = new float[n * n]{0};
+    auto it = initializer_list.begin();
+    for (int i = 0; i < n * n && it != initializer_list.end(); ++i, ++it) {
+      data[i] = *it;
+    }
   }
+
   ~Matrix() { delete[] data; }
 
   float &operator()(int row, int col);
--- test/testMatrix.cpp
+++ test/testMatrix.cpp
@@ -12,19 +12,19 @@ i \ j
   | 13.5| 14.5| 15.5| 16.5|
   */
 
-  Matrix A(4);
-  A(0, 0) = 1.0f;
-  assert((A(0, 0) == 1.0f) && "A[0,0] should be 1");
+  Matrix B(4);
+  B(0, 0) = 1.0f;
+  assert((B(0, 0) == 1.0f) && "B[0,0] should be 1");
 
-  std::cout << "A[0,0] = " << A(0, 0) << std::endl;
-  /*
-    assert((A[0][0] == 1.0f) && "A[0,0] should be 1");
-    assert((A[0][3] == 4.0f) && "A[0,3] should be 4");
-    assert((A[1][0] = 5.5f) && "A[1,0] should be 5.5");
-    assert((A[1][2] = 7.5f) && "A[1,2] should be 7.5");
-    assert((A[2][2] = 11.0f) && "A[2,2] should be 11");
-    assert((A[3][0] = 13.5f) && "A[3,0] should be 13.5");
-    assert((A[3][2] = 15.5f) && "A[3,2] should be 15.5");
-  */
+  Matrix A(4, {1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5,
+               16.5});
+  assert((A(0, 0) == 1.0f) && "A[0,0] should be 1");
+  assert((A(0, 3) == 4.0f) && "A[0,3] should be 4");
+  assert((A(1, 0) = 5.5f) && "A[1,0] should be 5.5");
+  assert((A(1, 2) = 7.5f) && "A[1,2] should be 7.5");
+  assert((A(2, 2) = 11.0f) && "A[2,2] should be 11");
+  assert((A(3, 0) = 13.5f) && "A[3,0] should be 13.5");
+  assert((A(3, 2) = 15.5f) && "A[3,2] should be 15.5");
+
   return;
 }
